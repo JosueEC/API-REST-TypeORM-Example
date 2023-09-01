8a335bf454660ac0a910526475d67164498d256d
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-31 21:21:24 -0600

    feat(user.controller): 🔨 Add route postUser and add User entity to the user.module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService){}
+
+  @Post()
+  public async postUser(@Body() user: CreateUserDto): Promise<User> {
+    return await this.userService.saveUser(user);
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
+  // Aqui especificamos con que modulos, controladores y entidades
+  // va a estar trabajando este servicio User. Es la misma idea, solo
+  // cambia la sintaxis dependiendo del ORM y BD o libreria que se
+  // este usando
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService]
 })
