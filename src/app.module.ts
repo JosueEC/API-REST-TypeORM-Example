5a5116407dfd6ab261be31349cc04b5467167dd7
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-31 19:51:02 -0600

    feat(app.module): 🔨 Add postgres database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'users_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // En esta parte nos
+      // pide agregar todas las clases que van a ser consideras como
+      // entidades, esta es una forma de establecer que tome como
+      // entidades a cualquier archivo con la extension que le indiquemos
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
