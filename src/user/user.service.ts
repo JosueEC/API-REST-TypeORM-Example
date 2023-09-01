c51848c8546f7ad2998cb1a0ecabc66ab8097050
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-31 21:07:01 -0600

    feat(user): 🔨 Add saveUser service and CreateUserDto

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private userRepository: Repository<User>){}
+
+  // Podemos devolver la promesa como tal, asi el controlador sera
+  // el que se encargue de manejar el codigo asincrono
+  public saveUser(user: CreateUserDto): Promise<User> {
+    const userCreated = this.userRepository.create(user);
+    return this.userRepository.save(userCreated);
+  }
+}
