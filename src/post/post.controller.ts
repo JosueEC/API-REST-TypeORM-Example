9cecf9e832c2edbef0b4a215328903637ca891af
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-02 13:56:33 -0600

    feat(post.controller): 🔨 Add createPost and getPost routes

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { SavePostDto } from './dto/save-post.dto';
+import { PostService } from './post.service';
 
 @Controller('/v1/post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Post()
+  public async createPost(@Body() post: SavePostDto) {
+    return await this.postService.savePost(post);
+  }
+
+  @Get()
+  public async getPost() {
+    return this.postService.findPost();
+  }
+}
