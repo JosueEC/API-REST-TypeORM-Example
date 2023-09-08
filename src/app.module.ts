75d422be95c51974f42d9f330b50d2bbb401fee6
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-09-07 18:58:08 -0600

    feat(app.module): 🔨 Add imports, controllers and profiders of occupation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,30 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PublicationModule } from './publication/publication.module';
+import { OccupationModule } from './occupation/occupation.module';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const dbConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'admin',
+  database: 'users_db',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'], // En esta parte nos
+  // pide agregar todas las clases que van a ser consideras como
+  // entidades, esta es una forma de establecer que tome como
+  // entidades a cualquier archivo con la extension que le indiquemos
+  //* [User, Publication, Occupation]
+  synchronize: true, // Esto va false en produccion
+};
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'users_db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], // En esta parte nos
-      // pide agregar todas las clases que van a ser consideras como
-      // entidades, esta es una forma de establecer que tome como
-      // entidades a cualquier archivo con la extension que le indiquemos
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(dbConfig),
     UserModule,
     PublicationModule,
+    OccupationModule,
   ],
   controllers: [],
   providers: [],
